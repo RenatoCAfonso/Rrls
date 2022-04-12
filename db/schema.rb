b9fb50074f3bd8f6b9f3fb85ae05926d12a63de0
Renato <r_cafonso@hotmail.com>
2022-04-11 23:21:39 -0300

    auth - password

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_11_205459) do
+ActiveRecord::Schema.define(version: 2022_04_11_213747) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,7 +26,6 @@ ActiveRecord::Schema.define(version: 2022_04_11_205459) do
   create_table "users", force: :cascade do |t|
     t.string "full_name"
     t.string "email"
-    t.string "password"
     t.string "location"
     t.text "bio"
     t.datetime "created_at", null: false
